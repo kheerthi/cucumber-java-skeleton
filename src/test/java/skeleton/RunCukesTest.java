6307d8a93b238f9df27c31d7a7073efdbca6c571
kheerthi <kheerthi.m@gmail.com>
2017-10-15 16:58:08 -0500

    Update RunCukesTest.java

--- src/test/java/skeleton/RunCukesTest.java
+++ src/test/java/skeleton/RunCukesTest.java
@@ -5,6 +5,6 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(format = {"pretty","html:target/output","json:target/output/cucumber.json"})
+@CucumberOptions(format = {"pretty","json:target/output/cucumber.json"})
 public class RunCukesTest {
 }
